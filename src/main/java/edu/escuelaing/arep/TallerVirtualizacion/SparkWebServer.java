8570489a5a75c8b0f3486aec8ed53aeee1bf504e
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-03-15 20:11:52 -0500

    Creando Readme y javadoc.

--- src/main/java/edu/escuelaing/arep/TallerVirtualizacion/SparkWebServer.java
+++ src/main/java/edu/escuelaing/arep/TallerVirtualizacion/SparkWebServer.java
@@ -1,16 +1,26 @@
 package edu.escuelaing.arep.TallerVirtualizacion;
 import static spark.Spark.*;
 /**
- * Hello world!
- *
+ * Esta clase define los subprocesos para empezar a ejecutrar.
+ * @author Deivid Medina
+ * @version 15/03/2022
  */
+
 public class SparkWebServer
 {
+    /**
+     * Clase principal main, la cual invoca los métodos correspondientes para ejecutar el main.
+     * @param args - String []
+     */
     public static void main( String[] args ){
         port(getPor());
         get("/hello", ((request, response) -> "Hello World Docker"));
     }
 
+    /**
+     * Método que me permite obtener el puerto donde se ejecutara la aplicación, en caso de no ser definido por defecto se dara el puerto 4567
+     * @return int - Retorna el puerto obtenido o sino por defecto el puerto 4567.
+     */
     private static int getPor() {
         if (System.getenv("PORT") != null){
             return Integer.parseInt(System.getenv("PORT"));
